f0a7b140bc561bd3a420a6f77c2ac836b76dc0ec
M Salman Khan <thesalmankhan2021@gmail.com>
2025-10-19 15:58:25 +0530

    chore: polish classSchema.js for better code readablity

--- server/classSchema.js
+++ server/classSchema.js
@@ -1,31 +1,10 @@
 class Datastore{
-    // constructor(){
-    //     this.data=[]
-    // }
-    // addItems(item){
-    //     this.data.push(item)
-    // }
-    // getAllItems(){
-    //     return this.data
-    // }
-    // getItemById(id){
-    //     return this.data.find(item=>item.id==id)
-    // }
-    // deleteItem(id){
-    //     this.data=this.data.filter(item=>item.id!==id)
-    // }
     constructor(id,name,cls,sub,grade){
         this.id=id
         this.name=name
         this.grade=grade
         this.sub=sub
         this.cls=cls
-    }
-    getDetails(){
-        return {name,grade,sub,cls}
-    }
-    inputDetails(){
-
     }
 }
 
